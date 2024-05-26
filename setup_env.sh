aec886cee96d4e276c75fb1876c5f29a0a700d48
Seb Burrell <sebburrell@outlook.com>
2024-05-26 17:19:29 +0100

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Check for required environment variables
-required_vars=("ORG_NAME" "PROJECT_NAME" "SOLUTION_NAME" "RELEASE_VERSION" "RELEASE_QUERY" "PAT" "GPT_API_KEY" "SOFTWARE_SUMMARY" "DESIRED_WORK_ITEM_TYPES" "OUTPUT_FOLDER")
-
-for var in "${required_vars[@]}"
-do
-  if [ -z "${!var}" ]; then
-    echo "Error: $var is not set."
-    exit 1
-  fi
-done
-
 # Create .env file
 cat <<EOF > .env
 ORG_NAME=$ORG_NAME
